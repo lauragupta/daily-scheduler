ee641fceb4436794cdfdc63332cccf8f50637b6f
Laura Gupta <95309002+lauragupta@users.noreply.github.com>
2022-01-07 18:03:08 -0800

    color coding for past present and future working

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
-function getDay() {
+function getDayAndTime() {
     var date = moment().format("dddd MMMM Do");
     $("#currentDay").text(date);
+    changeHour();
 }   
 
-setInterval(getDay, 1000);
+setInterval(getDayAndTime, 1000);
 
+function checkPastPresentFuture(i, textarea) {
+    var $textarea = $(textarea);
+    var militaryTimeID = parseInt($textarea.data("id"));
+    var currentHour = parseInt(moment().format("HH"));
+    if (currentHour === militaryTimeID) {
+        $textarea.addClass("present");
+    } else if (currentHour > militaryTimeID) {
+        $textarea.addClass("past");
+    } else if (currentHour < militaryTimeID){
+        $textarea.addClass("future");
+    }
+}
+
+function changeHour() {
+    $("textarea").each(checkPastPresentFuture);
+}
 

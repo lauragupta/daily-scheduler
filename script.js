bc0510e6799b71ad8bfb3dbddebf080d847f09e1
Laura Gupta <95309002+lauragupta@users.noreply.github.com>
2022-01-08 00:37:55 -0800

    render and init functions working

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
+function init() {
+    renderProjects();
+    setInterval(getDayAndTime, 1000);
+    $(".saveBtn").on("click", saveProject);
+}
+
+init();
+
 function getDayAndTime() {
     var date = moment().format("dddd MMMM Do");
     $("#currentDay").text(date);
     changeHour();
-    getProjects();
 }   
-
-setInterval(getDayAndTime, 1000);
 
 function getProjects() {
     var currentProjects = JSON.parse(localStorage.getItem("currentProjects"));
@@ -15,9 +20,12 @@ function getProjects() {
     return currentProjects
 }
 
-// function renderProjects() {
-
-// }
+function renderProjects() {
+    var currentProjects = getProjects();
+    $.each(currentProjects, function( key, value ) {
+        $(`textarea[data-id='${key}']`).val(value);
+      });
+}
 
 function checkPastPresentFuture(i, textarea) {
     var $textarea = $(textarea);
@@ -40,24 +48,7 @@ function saveProject(event) {
     var $clickedButton = $(event.target);
     var buttonID = $clickedButton.data("id");
     var savedProject = $clickedButton.prev().val();
-    console.log(buttonID);
-    console.log(savedProject);
-    currentProjects = getProjects();
+    var currentProjects = getProjects();
     currentProjects[buttonID] = savedProject;
     localStorage.setItem("currentProjects", JSON.stringify(currentProjects));
 }
-
-$(".saveBtn").on("click", saveProject);
-
- // var currentProjects = {
-    //     8: project8, 
-    //     9: project9,
-    //     10: project10,
-    //     11: project11,
-    //     12: project12,
-    //     13: project13,
-    //     14: project14,
-    //     15: project15, 
-    //     16: project16, 
-    //     17: project17,
-    // };

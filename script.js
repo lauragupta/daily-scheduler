69719332e36b0b56c3268c31d81552e668785e89
Laura Gupta <95309002+lauragupta@users.noreply.github.com>
2022-01-07 22:32:34 -0800

    change divs to buttons in html and get buttonid and project info to console log with on click.

--- script.js
+++ script.js
@@ -6,6 +6,10 @@ function getDayAndTime() {
 
 setInterval(getDayAndTime, 1000);
 
+function getProjects() {
+    var storedProjects = JSON.parse(localStorage.getItem(""))
+}
+
 function checkPastPresentFuture(i, textarea) {
     var $textarea = $(textarea);
     var militaryTimeID = parseInt($textarea.data("id"));
@@ -23,3 +27,25 @@ function changeHour() {
     $("textarea").each(checkPastPresentFuture);
 }
 
+function saveProject(event) {
+    var $clickedButton = $(event.target);
+    var buttonID = $clickedButton.data("id");
+    var savedProject = $clickedButton.prev().val();
+    // var currentProjects = {
+    //     8: project8, 
+    //     9: project9,
+    //     10: project10,
+    //     11: project11,
+    //     12: project12,
+    //     13: project13,
+    //     14: project14,
+    //     15: project15, 
+    //     16: project16, 
+    //     17: project17,
+    // };
+    console.log(buttonID);
+    console.log(savedProject);
+    
+}
+
+$(".saveBtn").on("click", saveProject);
